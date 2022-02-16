6737f05bf7f98a8db4998997c7218b12d44f3bf8
Anthony Bradburn <ajbradburn@gmail.com>
2022-02-16 05:48:34 -0500

    Added a python file that will format the code with line numbers so that I can include it in the documentation.

--- assembly_basics/p1.s
+++ assembly_basics/p1.s
@@ -2,7 +2,7 @@
 .func main
 
 main:
-  mov r0, #3
-  mov r1, #4
+  mov r0, #5
+  mov r1, #5
   add r0, r0, r1
   bx lr
---
+++ output_with_format.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import sys, getopt
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "hi:")
+    except getopt.GetoptError:
+        print('output_with_format.py -i <file_name>')
+        sys.exit(2)
+
+    input_file = ''
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print('output_with_format.py -i <file_name>')
+        elif opt in ("-i"):
+            input_file = arg
+
+    try:
+        open(input_file, "r")
+    except IOError:
+        print("Error: File does not exist?")
+        return 0
+
+        # We need to know what padding to apply.
+    lines = []
+    with open(input_file, "r") as f:
+        lines = f.readlines()
+    f.close()
+
+    line_count = len(lines)
+
+    padding = len("{}".format(line_count))
+    fstring = "{:>" + str(padding) + "}|  {}"
+
+    for i, line in enumerate(lines):
+        print(fstring.format(i, line.rstrip()))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
