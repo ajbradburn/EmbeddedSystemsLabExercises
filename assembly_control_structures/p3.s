f83d8037b44a7b6dc45b9246e3c2439009bda3bb
Anthony Bradburn <ajbradburn@gmail.com>
2022-02-18 15:41:36 -0500

    Completed the final exercise.

--- assembly_control_structures/p3.s
+++ assembly_control_structures/p3.s
@@ -1,5 +1,5 @@
 .data
-temp_measurement: .word 132
+temp_measurement: .word 130
 temp_upper: .word 150
 temp_lower: .word  100
 
@@ -8,9 +8,16 @@ temp_lower: .word  100
 error:
   /* Send a signal to a connected monitoring device. */
   /* Turn on a red LED. */
-  bx lr
+  mov r0, #0
+  b end
 
-cool:
+all_off:
+  and r3, r7, #6
+  eor r7, r7, r7
+  mov r0, #1
+  b end
+
+cool_on:
   /* Check if cool is already enabled. */
   and r3, r7, #2
   cmp r3, #2
@@ -18,9 +25,10 @@ cool:
   beq error
   /* if no, enable. */
   and r7, r7, #2
-  bx lr
+  mov r0, #2
+  b end
 
-heat:
+heat_on:
   /* Check if heat is already enabled. */
   and r3, r7, #4
   cmp r3, #4
@@ -28,25 +36,29 @@ heat:
   beq error
   /* if no, enable. */
   and r7, r7, #4
-  bx lr
-  bx lr
+  mov r0, #3
+  b end
 
 .global main
 main:
   /* Check temperature reading. */
   ldr r1, =temp_measurement
+  ldr r1, [r1]
 
   /* If equal or above temp_upper, call cool. */
   ldr r2, =temp_upper
   ldr r2, [r2]
+  /* cmp subtracts r1 from r2 */
   cmp r1, r2
-  bge cool
+  bhs cool_on
 
   /* If equal or lower temp_lower, call heat. */
   ldr r2, =temp_lower
   ldr r2, [r2]
   cmp r1, r2
-  ble heat
+  bls heat_on
 
   /* If neither, continue. */
+  b all_off
+end:
   bx lr
