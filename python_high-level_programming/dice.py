189a29c4af84b984cae0570b3b142fe9b918ab88
Anthony Bradburn <ajbradburn@gmail.com>
2022-02-23 14:25:58 -0500

    Finished the dice program.

--- python_high-level_programming/dice.py
+++ python_high-level_programming/dice.py
@@ -18,19 +18,23 @@ def calc_combinations(roll_totals, remaining_die, cumulative_rolls):
 def main(argv):
 # Initialize stats.
 # Check if configuration file exists.
-    file_name = 'die_stiats.json'
+    file_name = 'die_stats.json'
     if(exists(file_name)):
-        file_handle = fopen(file_name)
+        file_handle = open(file_name, 'r')
         stats = json.load(file_handle)
+        file_handle.close()
+        file_handle = open(file_name, 'w')
     else:
+        file_handle = open(file_name, 'w')
         stats = {
-                    1:{
-                        1:{"rolls":0, "probability":0.16},
-                        2:{"rolls":0, "probability":0.16},
-                        3:{"rolls":0, "probability":0.16},
-                        4:{"rolls":0, "probability":0.16},
-                        5:{"rolls":0, "probability":0.16},
-                        6:{"rolls":0, "probability":0.16},
+                    '1':{
+                        'run_count': 0,
+                        '1':{"rolls":0, "percentage":16},
+                        '2':{"rolls":0, "percentage":16},
+                        '3':{"rolls":0, "percentage":16},
+                        '4':{"rolls":0, "percentage":16},
+                        '5':{"rolls":0, "percentage":16},
+                        '6':{"rolls":0, "percentage":16},
                     }
                 }
 
@@ -56,18 +60,29 @@ def main(argv):
     
     # Make sure that any input is an int.
     number_of_die = int(number_of_die)
+    str_number_of_die = str(number_of_die)
 
+    # Check the user input for a reasonable value.
     if number_of_die < 1:
         print('You must enter a number greater than 1.')
         sys.exit(2)
 
-    if not number_of_die in stats:
+    # Increment our run counter.
+
+    if not str_number_of_die in stats:
         die_combinations = []
         calc_combinations(die_combinations, number_of_die, 0)
-# TODO
-# Add the statistics changes to the stats dictionary.
+        roll_values = (list(set(die_combinations)))
 
-# Roll Die.
+        stats[str_number_of_die] = {}
+        stats[str_number_of_die]['run_count'] = 0
+        for rv in roll_values:
+            roll_possibilities = len(die_combinations)
+            roll_frequency = die_combinations.count(rv)
+            roll_percentage = int(roll_frequency / roll_possibilities * 100)
+            stats[str_number_of_die][str(rv)] = {"rolls":0, "percentage":roll_percentage}
+
+    # Roll Die.
     print("Rolling {} die.".format(number_of_die))
 
     rolls = {}
@@ -75,16 +90,27 @@ def main(argv):
 
     for d in range(1, number_of_die + 1):
         rolls[d] = random.randint(1, 6)
-        stats[1][rolls[d]]['rolls'] = stats[1][rolls[d]]['rolls'] + 1
+        str_roll = str(rolls[d])
+        stats['1'][str_roll]['rolls'] = stats['1'][str_roll]['rolls'] + 1
+        stats['1']['run_count'] = stats['1']['run_count'] + 1
         roll_total = roll_total + rolls[d]
+    str_roll_total = str(roll_total)
+    stats[str_number_of_die][str_roll_total]['rolls'] = stats[str_number_of_die][str_roll_total]['rolls'] + 1
+    stats[str_number_of_die]['run_count'] = stats[str_number_of_die]['run_count'] + 1
 
-# Output die results.
-    print(rolls)
-    print(stats)
+    # Output die results.
+    for d in rolls:
+        print("Die {} rolled a {}.".format(d, rolls[d]))
+
+    str_actual_roll_percent = int((stats[str_number_of_die][str_roll_total]['rolls'] / stats[str_number_of_die]['run_count']) * 100)
+    print("Out of the {} times that this program has been run {} has been rolled {} times, a percentage of {}. A roll of {} should occur {}% of the time.".format(stats[str_number_of_die]['run_count'], str_roll_total, stats[str_number_of_die][str_roll_total]['rolls'], str_actual_roll_percent, str_roll_total, stats[str_number_of_die][str_roll_total]['percentage']))
 
 # Output die statistics.
 
 # Save config and statistics to a file.
+    stats_string = json.dumps(stats, indent=4)
+    file_handle.truncate()
+    file_handle.write(stats_string)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
