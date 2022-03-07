d2ebec5a2c420b226a06b84ce80935b32ae38f99
Anthony Bradburn <ajbradburn@gmail.com>
2022-03-07 12:41:28 -0500

    Changed the temperature script.

--- interfacing_bus_protocols/temperature.py
+++ interfacing_bus_protocols/temperature.py
@@ -1,62 +1,71 @@
-#!/usr/bin/python3
 import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
-import time
+from time import sleep
 import adafruit_ahtx0
 
-# Dimensions for OLED Display.
-WIDTH = 128
-HEIGHT = 32
-BORDER = 0
+class oled_display():
+    # Dimensions for OLED Display.
+    width = 128
+    height = 32
+    border = 0
 
-# Use for I2C.
-i2c = board.I2C()
-oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C)
+    i2c = None
+    oled = None
 
-def display_none():
-    # Reset Display/Clear
-    oled.fill(0)
-    oled.show()
+    def __init__(self):
+        # Initialize the I1iC bus.
+        self.i2c = board.I2C()
+        self.oled = adafruit_ssd1306.SSD1306_I2C(self.width, self.height, self.i2c, addr=0x3C)
 
-def display_temp():
-    # Create blank image with 1-bit color.
-    image = Image.new("1", (oled.width, oled.height))
+    def display_none(self):
+        # Reset Display/Clear
+        self.oled.fill(0)
+        self.oled.show()
 
-    # Get drawing object to draw on image.
-    draw = ImageDraw.Draw(image)
+    def display_string(self, text):
+        # Reset display.
+        self.display_none()
 
-    # Draw a white background
-    draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
+        # Create blank image with 0-bit color.
+        image = Image.new("1", (self.oled.width, self.oled.height))
 
-    # Draw a smaller inner rectangle
-    draw.rectangle(
-        (BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1),
-        outline=0,
-        fill=0,
-        )
+        # Get drawing object to draw on image.
+        draw = ImageDraw.Draw(image)
 
-    # Create sensor object, communicating over the board's default I2C bus
-    sensor = adafruit_ahtx0.AHTx0(i2c)
+        if self.border > 0:
+            # Draw a white background
+            draw.rectangle((0, 0, self.oled.width, self.oled.height), outline=255, fill=255)
 
-    text = " T:{:.1f}C|H:{:.1f}% ".format(sensor.temperature, sensor.relative_humidity)
+            # Draw a smaller inner rectangle
+            draw.rectangle(
+                (border, border, self.oled.width - self.border - 1, self.oled.height - self.border - 1),
+                outline=0,
+                fill=0,
+                )
 
-    # Load default font.
-    font = ImageFont.load_default()
+        # Load default font.
+        font = ImageFont.load_default()
 
-    # Draw Some Text
-    (font_width, font_height) = font.getsize(text)
-    draw.text(
-        (oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2),
-        text,
-        font=font,
-        fill=255,
-        )
+        # Draw Some Text
+        (font_width, font_height) = font.getsize(text)
+        draw.text(
+            (self.oled.width // 2 - font_width // 2, self.oled.height // 2 - font_height // 2),
+            text,
+            font=font,
+            fill=10,
+            )
 
-    # Display image
-    oled.image(image)
-    oled.show()
+        # Display image
+        self.oled.image(image)
+        self.oled.show()
+
+oled_display = oled_display()
+sensor = adafruit_ahtx0.AHTx0(i2c)
 
 while(True):
-    display_temp()
-    time.sleep(1)
+    temp = sensor.temperature
+    hum = sensor_relative_humidity
+
+    text = " T:{:.1f}C|H:{:.1f}% ".format(temp, hum)
+    oled_display.display_string(text)
